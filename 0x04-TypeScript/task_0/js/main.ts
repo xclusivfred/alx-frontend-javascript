01a05f89604535094c5364e43f577c88e8547f5e
xclusivfred <xclusivfred@gmail.com>
2023-07-05 06:42:04 +0100

    new folder 0x04

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,20 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'Bendito',
+    lastName: 'Francis',
+    age: 69,
+    location: 'Lagos',
+}
+
+const student2: Student = {
+    firstName: 'Amoroso',
+    lastName: 'Akpanyung',
+    age: 96,
+    location: 'Lagos',
+}
